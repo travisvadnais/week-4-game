b4f6d3f9b3b639be8be691fe64560051ee8e2ed3
Travis Vadnais <travisvadnais@gmail.com>
2019-01-06 10:37:37 -0500

    convert to ES6; DRY up code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,104 +1,82 @@
-$(document).ready(function () {
+$(document).ready(() => {
 
-var wins;
-var losses;
-var valFirst;
-var valSecond;
-var valThird;
-var valFourth;
-var gameVal;
-var spotUsed=false;
+  let wins;
+  let losses;
+  let valFirst;
+  let valSecond;
+  let valThird;
+  let valFourth;
+  let gameVal;
+  let spotUsed = false;
 
-    function resetGame() {
-        valFirst = generateRandom(1, 12);
-        valSecond = generateRandom(1, 12);
-        valThird = generateRandom(1, 12);
-        valFourth = generateRandom(1, 12);
-        currentWeight = 0;
-        updateCurrentWeight();
-        wins = 0;
-        losses = 0;
-        spotUsed=false;
-        gameVal = generateRandom(19, 102);
-        $(".bigBoldGameVal").html(gameVal);
+  const resetGame = () => {
+    softReset();
+    wins = 0;
+    losses = 0;
+    spotUsed=false;
+  };
+  const softReset = () => {
+    valFirst = generateRandom(1, 12);
+    valSecond = generateRandom(1, 12);
+    valThird = generateRandom(1, 12);
+    valFourth = generateRandom(1, 12);
+    currentWeight = 0;
+    updateCurrentWeight();
+    gameVal = generateRandom(19, 102);
+    $(".bigBoldGameVal").html(gameVal);
+  };
+  const generateRandom = (a, b) => {return Math.floor(Math.random() * b) + a};
+  const updateCurrentWeight = () => {$(".bigBoldCurrentWeight").html(currentWeight)};
+  
+  const calculateScore = () => {
+    if (currentWeight === gameVal) {
+      wins++;
+      $(".bigBoldWins").html(wins);    
+      softReset();
     }
-    function softReset() {
-        valFirst = generateRandom(1, 12);
-        valSecond = generateRandom(1, 12);
-        valThird = generateRandom(1, 12);
-        valFourth = generateRandom(1, 12);
-        currentWeight = 0;
-        updateCurrentWeight();
-        gameVal = generateRandom(19, 102);
-        $(".bigBoldGameVal").html(gameVal);
-
+    else if (currentWeight > gameVal) {
+      losses++;
+      $(".bigBoldLosses").html(losses);    
+      softReset();
     }
-    function generateRandom(a, b) {
-        return Math.floor(Math.random() * b) + a;
+  };          
+
+  resetGame();
+
+  $(".rounded-circle").on("click", ((event) => {
+    let plateId = event.target.id;
+    let plateVal;
+    switch (plateId) {
+      case "plate1":
+        plateVal = valFirst;
+        break;
+      case "plate2":
+        plateVal = valSecond;
+        break;
+      case "plate3":
+        plateVal = valThird;
+        break;
+      default:
+        plateVal = valFourth;
     }
+    addWeight(plateVal);
+  }));
 
-    function updateCurrentWeight() {
-        $(".bigBoldCurrentWeight").html(currentWeight);
+  let addWeight = val => {
+    currentWeight += val;
+    updateCurrentWeight();
+    calculateScore();
+  };
+
+  $('#getSpot').on('click', (() => {
+    switch (spotUsed) {
+      case false: 
+        softReset();
+        spotUsed=true;
+        $().button('dispose')
+        break;
+      default:
+        alert("You're on your own Bro!!!");
     }
-
-    function calculateScore() {
-        if (currentWeight === gameVal) {
-            wins++;
-            $(".bigBoldWins").html(wins);    
-            softReset();
-        }
-        else if (currentWeight > gameVal) {
-            losses++;
-            $(".bigBoldLosses").html(losses);    
-            softReset();
-        }
-    }           
-    
-
-    resetGame();
-
-    $(".plate1").on('click', function() {
-        currentWeight += valFirst;
-        updateCurrentWeight();
-        calculateScore();        
-    });
-
-    $(".plate2").on('click', function() {
-        currentWeight += valSecond;
-        updateCurrentWeight();
-        calculateScore();
-    });
-
-    $(".plate3").on('click', function() {
-        currentWeight += valThird;
-        updateCurrentWeight();
-        calculateScore();
-    });
-
-    $(".plate4").on('click', function() {
-        currentWeight += valFourth;
-        updateCurrentWeight();
-        calculateScore();
-    });
-
-    $('#getSpot').on('click', function() {
-        switch (spotUsed) {
-            case false: 
-                softReset();
-                spotUsed=true;
-                $().button('dispose')
-                break;
-            default:
-                alert("You're on your own Bro!!!");
-        }
-    });
-        
-
-
-
-
-
-
-
-
+  }));
 });
